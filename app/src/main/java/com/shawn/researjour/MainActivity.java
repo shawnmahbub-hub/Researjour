321e0bce21818536a7b2ab4eb3fad7f41cb9bb4e
Shawn Mahbub <sawon78656@gmail.com>
2020-01-16 16:06:01 +0600

    Splash Screen Activity has been completed. Need to change the app_icon and app logo.gif

--- app/src/main/java/com/shawn/researjour/MainActivity.java
+++ app/src/main/java/com/shawn/researjour/MainActivity.java
@@ -2,13 +2,31 @@ package com.shawn.researjour;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        /* New Handler to start the Menu-Activity
+         * and close this Splash-Screen after some seconds.*/
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this, Welcome_Screen.class);
+                MainActivity.this.startActivity(intent);
+                MainActivity.this.finish();
+            }
+        },SPLASH_DISPLAY_LENGTH);
     }
 }
---
+++ app/src/main/java/com/shawn/researjour/Welcome_Screen.java
@@ -0,0 +1,14 @@
+package com.shawn.researjour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Welcome_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome__screen);
+    }
+}
