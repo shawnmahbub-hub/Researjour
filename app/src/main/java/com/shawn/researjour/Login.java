59f412f9d7707061d67b0aea00edb304cdaff25f
Shawn Mahbub <sawon78656@gmail.com>
2020-01-20 13:53:26 +0600

    Disability added with the login button

--- app/src/main/java/com/shawn/researjour/Login.java
+++ app/src/main/java/com/shawn/researjour/Login.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
+    EditText email, password;
     TextView forgot_pass, sign_up;
     Button login;
 
@@ -23,6 +27,12 @@ public class Login extends AppCompatActivity {
         forgot_pass=findViewById(R.id.forgot_pass_id);
         sign_up=findViewById(R.id.sign_up_text_id);
         login=findViewById(R.id.login_button_id);
+        email=findViewById(R.id.emai_editText_id);
+        password=findViewById(R.id.password_editText_id);
+
+        /*login text watcher for empty edit text field*/
+        email.addTextChangedListener(loginTextWatcher);
+        password.addTextChangedListener(loginTextWatcher);
 
         forgot_pass.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,4 +61,26 @@ public class Login extends AppCompatActivity {
             }
         });
     }
+
+    /*method for login text watcher*/
+    private TextWatcher loginTextWatcher=new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String userNameInput=email.getText().toString().trim();
+            String passwordInput=password.getText().toString().trim();
+
+            //setting the button enabled if the edit text field is not empty
+            login.setEnabled(!userNameInput.isEmpty() && !passwordInput.isEmpty());
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
 }
