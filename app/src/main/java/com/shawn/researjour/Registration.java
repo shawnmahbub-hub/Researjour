a175f88cc79e4527a05d74c48d0d727e3f5a71ee
Shawn Mahbub <sawon78656@gmail.com>
2020-01-21 15:23:20 +0600

    terms and conditions activity added

--- app/src/main/java/com/shawn/researjour/Registration.java
+++ app/src/main/java/com/shawn/researjour/Registration.java
@@ -22,9 +22,9 @@ public class Registration extends AppCompatActivity {
     ImageButton back_button,google,fb,phone;
     EditText firstName, lastName, email, password, confirmPassword;
     Button registration;
-    TextView login;
+    TextView login,terms;
     CheckBox checkBox;
-    boolean flag = false;
+    boolean flag;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,32 +53,34 @@ public class Registration extends AppCompatActivity {
         firstName=findViewById(R.id.firstName_editText_id);
         lastName=findViewById(R.id.lastName_editText_id);
         email=findViewById(R.id.emai_editText_id);
+        terms=findViewById(R.id.Terms_id);
         password=findViewById(R.id.password_editText_id);
         confirmPassword=findViewById(R.id.confirmPassword_editText_id);
         registration=findViewById(R.id.reg_button_id);
         checkBox=findViewById(R.id.checkbox_id);
 
-        /*login text watcher for empty edit text field*/
-        firstName.addTextChangedListener(loginTextWatcher);
-        lastName.addTextChangedListener(loginTextWatcher);
-        email.addTextChangedListener(loginTextWatcher);
-        password.addTextChangedListener(loginTextWatcher);
-        confirmPassword.addTextChangedListener(loginTextWatcher);
+        terms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Registration.this, Terms.class);
+                startActivity(intent);
+            }
+        });
+
 
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-
-                String emailInput=email.getText().toString().trim();
+                String emailInput = email.getText().toString().trim();
                 String passwordInput=password.getText().toString().trim();
                 String firstNameInput=firstName.getText().toString().trim();
                 String lastNameInput=lastName.getText().toString().trim();
                 String confirmPassInput=confirmPassword.getText().toString().trim();
 
                 if (b){
+                    flag = true;
                     registration.setEnabled(!emailInput.isEmpty() && !passwordInput.isEmpty() && !firstNameInput.isEmpty()
                             && !lastNameInput.isEmpty() && !confirmPassInput.isEmpty() && flag);
-                    flag = true;
                 }else {
                     registration.setEnabled(false);
                     flag = false;
@@ -86,31 +88,10 @@ public class Registration extends AppCompatActivity {
                 flag = false;
             }
         });
+
     }
 
-    /*method for login text watcher*/
-    private TextWatcher loginTextWatcher=new TextWatcher() {
-        @Override
-        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
-        }
 
-        @Override
-        public void onTextChanged(CharSequence s, int start, int before, int count) {
-            String emailInput=email.getText().toString().trim();
-            String passwordInput=password.getText().toString().trim();
-            String firstNameInput=firstName.getText().toString().trim();
-            String lastNameInput=lastName.getText().toString().trim();
-            String confirmPassInput=confirmPassword.getText().toString().trim();
 
-            //setting the button enabled if the edit text field is not empty
-            registration.setEnabled(!emailInput.isEmpty() && !passwordInput.isEmpty() && !firstNameInput.isEmpty()
-            && !lastNameInput.isEmpty() && !confirmPassInput.isEmpty());
-        }
-
-        @Override
-        public void afterTextChanged(Editable s) {
-
-        }
-    };
 }
