b3e59f660925789638276527dfe36457ae9db143
Shawn Mahbub <sawon78656@gmail.com>
2020-01-26 17:08:13 +0600

    validation has been done

--- app/src/main/java/com/shawn/researjour/Registration.java
+++ app/src/main/java/com/shawn/researjour/Registration.java
@@ -1,12 +1,9 @@
 package com.shawn.researjour;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -18,15 +15,6 @@ import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class Registration extends AppCompatActivity {
 
@@ -34,25 +22,16 @@ public class Registration extends AppCompatActivity {
     inside the registration activity
      */
     ImageButton back_button,google,fb,phone;
-    EditText firstName, lastName, reg_email, password, confirmPassword;
+    EditText firstName, lastName, email, password, confirmPassword;
     Button registration;
     TextView login,terms;
     CheckBox checkBox;
     boolean flag;
-    ProgressDialog loadingBar;
-
-    //initiating firebaseauthencating for registering user
-    FirebaseAuth mAuth;
-    //DatabaseReference userReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-
-        mAuth=FirebaseAuth.getInstance();
-        //userReference=FirebaseDatabase.getInstance().getReference();
-        loadingBar=new ProgressDialog(this);
 
         //finding the id's of the components
         back_button=findViewById(R.id.back_btn);
@@ -75,7 +54,7 @@ public class Registration extends AppCompatActivity {
 
         firstName=findViewById(R.id.firstName_editText_id);
         lastName=findViewById(R.id.lastName_editText_id);
-        reg_email=findViewById(R.id.reg_email_editText_id);
+        email=findViewById(R.id.emai_editText_id);
         terms=findViewById(R.id.Terms_id);
         password=findViewById(R.id.password_editText_id);
         confirmPassword=findViewById(R.id.confirmPassword_editText_id);
@@ -88,20 +67,19 @@ public class Registration extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                String emailInput = reg_email.getText().toString().trim();
+                String emailInput = email.getText().toString().trim();
                 String passwordInput=password.getText().toString().trim();
                 String firstNameInput=firstName.getText().toString().trim();
                 String lastNameInput=lastName.getText().toString().trim();
                 String confirmPassInput=confirmPassword.getText().toString().trim();
-                //FirebaseUser currentUser=mAuth.getCurrentUser();
 
                 if(emailInput.isEmpty()){
-                    reg_email.setError("Email field can't be empty");
-                    reg_email.requestFocus();
+                    email.setError("Email field can't be empty");
+                    email.requestFocus();
                     return;
                 }else if (!Patterns.EMAIL_ADDRESS.matcher(emailInput).matches()){
-                    reg_email.setError("Invalid Email");
-                    reg_email.requestFocus();
+                    email.setError("Invalid Email");
+                    email.requestFocus();
                     return;
 
                 } else if (passwordInput.isEmpty()){
@@ -120,42 +98,17 @@ public class Registration extends AppCompatActivity {
                     lastName.setError("No Last Name");
                     lastName.requestFocus();
                     return;
-                }else if (passwordInput.length()<6){
+                }if (passwordInput.length()<6){
                     password.setError("Password should be 6-12 characters long");
                     password.requestFocus();
                     return;
-                }else if (!passwordInput.equals(confirmPassInput)){
+                }if (!passwordInput.equals(confirmPassInput)){
                     confirmPassword.setError("Password don't match");
                     confirmPassword.requestFocus();
                     return;
-                }else {
-                    //showing the progress loading bar
-                    loadingBar.setTitle("Creating New Account");
-                    loadingBar.setMessage("Wait for a moment while we are connecting with you");
-                    loadingBar.show();
-                    loadingBar.setCanceledOnTouchOutside(true);
-
-                    if (!isNetworkConnected()==true){
-                        Toast.makeText(Registration.this, "No Connection", Toast.LENGTH_SHORT).show();
-                    }
-
-                    mAuth.createUserWithEmailAndPassword(emailInput,passwordInput).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()){
-
-                                //calling the method
-                                SendUserToViewPagerActivity();
-                                Toast.makeText(Registration.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
-                                loadingBar.dismiss();
-                            }else {
-                                String message=task.getException().getMessage();
-                                Toast.makeText(Registration.this, "Error: "+message, Toast.LENGTH_SHORT).show();
-                                loadingBar.dismiss();
-                            }
-                        }
-                    });
                 }
+                Intent intent=new Intent(Registration.this, IntroActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -167,18 +120,19 @@ public class Registration extends AppCompatActivity {
             }
         });
 
+
         checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                String reg_emailInput=reg_email.getText().toString().trim();
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                String emailInput = email.getText().toString().trim();
                 String passwordInput=password.getText().toString().trim();
                 String firstNameInput=firstName.getText().toString().trim();
                 String lastNameInput=lastName.getText().toString().trim();
                 String confirmPassInput=confirmPassword.getText().toString().trim();
 
-                if (isChecked){
+                if (b){
                     flag = true;
-                    registration.setEnabled(!reg_emailInput.isEmpty() && !passwordInput.isEmpty() && !firstNameInput.isEmpty()
+                    registration.setEnabled(!emailInput.isEmpty() && !passwordInput.isEmpty() && !firstNameInput.isEmpty()
                             && !lastNameInput.isEmpty() && !confirmPassInput.isEmpty() && flag);
                 }else {
                     registration.setEnabled(false);
@@ -188,19 +142,5 @@ public class Registration extends AppCompatActivity {
             }
         });
 
-    }
-
-    private void SendUserToViewPagerActivity() {
-
-        Intent intent=new Intent(Registration.this, IntroActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
-        startActivity(intent);
-
-
-    }
-
-    private boolean isNetworkConnected() {
-        ConnectivityManager connectivityManager=(ConnectivityManager) getSystemService(Registration.CONNECTIVITY_SERVICE);
-        return connectivityManager.getActiveNetworkInfo() !=null && connectivityManager.getActiveNetworkInfo().isConnected();
     }
 }
